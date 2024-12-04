fe69e2a9f204c6a35db74152da19b5983f8f3854
blue-beaker <yyy5566778899@126.com>
2024-12-04 18:32:38 +0800

    add ignoring feature

--- ffmpeg_hevc_cpu.py
+++ ffmpeg_hevc_cpu.py
@@ -45,11 +45,39 @@ def convert(inname,outname,crf=28,targetSize=0):
 def addToQueue(infile,outfile):
     queue.append([infile,outfile])
 
+def readIgnoreList(folder):
+    ignore_file=os.path.join(folder,".convert_ignore")
+    if(os.path.exists(ignore_file)):
+        with open(ignore_file, "r") as f:
+            list1=[]
+            for line in f.readlines():
+                if(line not in list1):
+                    list1.append(line.removesuffix("\n"))
+            return list1
+    return []
+
+def addFileToIgnoreList(file):
+    folder=os.path.dirname(file)
+    ignore_file=os.path.join(folder,".convert_ignore")
+    with open(ignore_file,"a") as f:
+        f.write(os.path.basename(file)+"\n")
+    
+        
+
 def queueFiles(file):
     name=os.path.basename(file)
-    if name!="converted" and os.path.isdir(file):
+
+    ignores=readIgnoreList(file)
+
+    if os.path.basename(file) in ignores:
+        print("ignored from .convert_ignore: "+file)
+    elif name!="converted" and os.path.isdir(file):
         for subpath in sorted(os.listdir(file)):
-            queueFiles(os.path.join(file,subpath))
+            if(not subpath in ignores):
+                queueFiles(os.path.join(file,subpath))
+            else:
+                print(f"ignored from {os.path.join(file,'.convert_ignore')}: "+os.path.join(file,subpath))
+
     elif file.endswith(".mkv") or file.endswith(".mp4"):
         if(convertAlreadyHEVC or not checkHEVC(file)):
             if not dry_run:
@@ -57,6 +85,7 @@ def queueFiles(file):
                 os.chown(os.path.join(os.path.dirname(file),"converted"),uid,gid)
             addToQueue(file,os.path.join(os.path.dirname(file),"converted",name))
         else:
+            addFileToIgnoreList(file)
             print(f"{file} is already in HEVC, skipping")
     else:
         if verbosity:
@@ -89,10 +118,14 @@ def convertInQueue():
         size=os.stat(item[0]).st_size
         print(f"\033[0;37;44m{item[0]} {getSizeStr(size).removeprefix('+')}\033[0m")
         result=convert(item[0],item[1],targetSize=size)
+
         size2=os.stat(item[1]).st_size
         if result==2:
             print(f"\033[0;37;41mCancelled: {i}/{queue.__len__()} '{item[0]}'=>'{item[1]}'\033[0m")
             return
+        if result==0:
+            addFileToIgnoreList(item)
+
         ratio=size2/size*100
         differString=getSizeStr(size2-size)
         converted.append([item[0],item[1],differString,f"{ratio:.1f}%"])
